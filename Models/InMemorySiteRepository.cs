b3daf04141d3e05916dad1c997914960f1a94504
Jamie Ide <jamie.ide@gmail.com>
2018-12-19 10:49:56 -0500

    Added Millers For A Century

--- Models/InMemorySiteRepository.cs
+++ Models/InMemorySiteRepository.cs
@@ -18,7 +18,8 @@ namespace EtAndHkIde.Models
             new Page("Recollections", "Recollections", new DateTime(2018, 12, 17)),
             new Page("MyGrandfather", "My Grandfather: A Biographical Sketch", new DateTime(2018, 12, 17)),
             new Page("ETIdeInIndiana", "E. T. Ide in Indiana", new DateTime(2018, 12, 17)),
-            new Page("InMemoryOfCompanionHoraceKnightIde", "In Memory Of Companion Horace Knight Ide", new DateTime(2018, 12, 17))
+            new Page("InMemoryOfCompanionHoraceKnightIde", "In Memory Of Companion Horace Knight Ide", new DateTime(2018, 12, 17)),
+            new Page("MillersForACentury", "Millers For A Century", new DateTime(2018, 12, 19))
         };
 
         private readonly IEnumerable<ImageCollection> _imageCollections = new List<ImageCollection>()
