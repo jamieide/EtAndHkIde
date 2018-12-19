f1035e845e0ebeda07fa4ecb589a5781cbdfefdb
Jamie Ide <jamie.ide@gmail.com>
2018-12-19 10:10:49 -0500

    WIP

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ namespace EtAndHkIde
         {
             if (env.IsDevelopment())
             {
+                app.UseStatusCodePages();
                 app.UseDeveloperExceptionPage();
             }
             else
