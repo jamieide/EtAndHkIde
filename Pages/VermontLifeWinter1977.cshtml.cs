149cae156a2c9b405bdad65d314f9a8293cc4f58
Jamie Ide <jamie.ide@gmail.com>
2018-12-21 16:06:42 -0500

    Added partial for Vermont Life Winter 1977

---
+++ Pages/VermontLifeWinter1977.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using EtAndHkIde.Infrastructure;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace EtAndHkIde.Pages
+{
+    public class VermontLifeWinter1977Model : PageModel, IContent
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public DateTime? PublishDate { get; }
+    }
+}
