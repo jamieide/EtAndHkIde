e907407737253bd1da6e0110f0ccb1c889604a01
Jamie Ide <jamie.ide@gmail.com>
2018-12-28 16:21:43 -0500

    added site index

---
+++ Pages/SiteIndex.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EtAndHkIde.Pages
+{
+    public class SiteIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
