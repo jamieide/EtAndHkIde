c92f92d4dc138021cf2de7b593654d1af0206c9c
Jamie Ide <jamie.ide@gmail.com>
2019-01-02 11:08:17 -0500

    WIP

--- Pages/Articles/LargeFire1883.cshtml.cs
+++ Pages/Articles/LargeFire1883.cshtml.cs
@@ -8,7 +8,7 @@ namespace EtAndHkIde.Pages.Articles
         public LargeFire1883Model()
         {
             Title = "Large Fire 1883";
-            Description = "A newspaper article describing the fire that consumed the original Passumpsic mill in 1883";
+            Description = "A newspaper article describing the fire that consumed the original Passumpsic mill in 1883.";
             PublishDate = new DateTime(2018, 12, 21);
             Tags = new[] { Tag.Passumpsic };
         }
---
+++ Pages/Shared/SidebarCitationViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace EtAndHkIde.Pages.Shared
+{
+    public class SidebarCitationViewModel
+    {
+        public enum CitationType
+        {
+            Undefined,
+            Book,
+            Magazine,
+            Newspaper
+        }
+
+        public SidebarCitationViewModel(CitationType type, string url, string linkText)
+        {
+            Type = type;
+            Url = url;
+            LinkText = linkText;
+        }
+
+        public CitationType Type { get; set; }
+        public string Url { get; set; }
+        public string LinkText { get; set; }
+    }
+}
