c12330714f15554fbe76952b8eefe758f005c1fe
Jamie Ide <jamie.ide@gmail.com>
2019-01-05 12:00:46 -0500

    Added drafts page

---
+++ Pages/Drafts.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using EtAndHkIde.Infrastructure;
+using EtAndHkIde.ViewComponents;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EtAndHkIde.Pages
+{
+    public class DraftsModel : PageModel
+    {
+        private readonly ISiteRepository _siteRepository;
+
+        public DraftsModel(ISiteRepository siteRepository)
+        {
+            _siteRepository = siteRepository;
+        }
+
+        public PageListCardViewModel PageList { get; set; }
+
+        public void OnGet()
+        {
+            var pages = _siteRepository.GetDraftPages();
+            PageList = new PageListCardViewModel("Drafts", pages);
+        }
+    }
+}
