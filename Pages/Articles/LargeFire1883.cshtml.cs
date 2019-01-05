b66ad6bedca51da40cc85e09306e12fd54e6e28b
Jamie Ide <jamie.ide@gmail.com>
2019-01-05 11:49:21 -0500

    Fixed titles

--- Pages/Articles/LargeFire1883.cshtml.cs
+++ Pages/Articles/LargeFire1883.cshtml.cs
@@ -5,7 +5,7 @@ namespace EtAndHkIde.Pages.Articles
 {
     public class LargeFire1883Model : SitePageModel
     {
-        public LargeFire1883Model() : base("Large Fire 1883")
+        public LargeFire1883Model() : base("Large Fire")
         {
             Description = "A newspaper article describing the fire that consumed the original Passumpsic mill in 1883.";
             PublishDate = new DateTime(2018, 12, 21);
