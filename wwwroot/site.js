154d0ca46270c108b2527415fa639c8d82586f65
Jamie Ide <jamie.ide@gmail.com>
2019-01-09 15:00:53 -0500

    Added JS to add rel=noopener to target=_blank links

--- wwwroot/site.js
+++ wwwroot/site.js
@@ -1,2 +1,6 @@
 ﻿(function($) {
+    $(document).ready(function() {
+        // add noopener to all target=_blank links, see https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/
+        $("a[target=_blank]").attr("rel", "noopener");
+    });
 }(jQuery));
