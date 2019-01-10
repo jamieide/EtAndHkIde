2513cd05ee4ea31aa264028a7270b8e1ef219699
Jamie Ide <jamie.ide@gmail.com>
2019-01-10 15:43:06 -0500

    centralized icons

---
+++ Infrastructure/Icons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EtAndHkIde.Infrastructure
+{
+    public static class Icons
+    {
+        public const string Home = "fas fa-home";
+        public const string Articles = "fas fa-newspaper";
+        public const string Blog = "fas fa-bullhorn";
+        public const string Galleries = "fas fa-images";
+        public const string Tags = "fas fa-tag";
+        public const string About = "fas fa-info";
+
+        public static string ForTag(string tagValue)
+        {
+            if (tagValue == TagValues.Featured)
+            {
+                return "tag fas fa-award";
+            }
+            if (TagValues.PersonTags.Contains(tagValue))
+            {
+                return "tag fas fa-user";
+            }
+            if (TagValues.PlaceTags.Contains(tagValue))
+            {
+                return "tag fas fa-map-pin";
+            }
+            return "tag fas fa-tag";
+        }
+    }
+}
--- Infrastructure/TagValues.cs
+++ Infrastructure/TagValues.cs
@@ -7,10 +7,18 @@ namespace EtAndHkIde.Infrastructure
     public static class TagValues
     {
         // System
-        public static readonly ISet<string> SystemTags = new HashSet<string>() { Featured };
         public const string Featured = "Featured";
+        public static readonly ISet<string> SystemTags = new HashSet<string>() { Featured };
 
         // People
+        public const string TimothyIde = "Timothy Ide";
+        public const string JacobIde = "Jacob Ide";
+        public const string ElmoreIde = "Elmore Ide";
+        public const string HoraceIde = "Horace Ide";
+        public const string WilliamIde = "William Ide";
+        public const string RichardIde = "Richard Ide";
+        public const string TimIde = "Tim Ide";
+        public const string GeorgeGray = "George Gray";
         public static readonly ISet<string> PersonTags = new HashSet<string>()
         {
             TimothyIde,
@@ -22,35 +30,10 @@ namespace EtAndHkIde.Infrastructure
             TimIde,
             GeorgeGray
         };
-        public const string TimothyIde = "Timothy Ide";
-        public const string JacobIde = "Jacob Ide";
-        public const string ElmoreIde = "Elmore Ide";
-        public const string HoraceIde = "Horace Ide";
-        public const string WilliamIde = "William Ide";
-        public const string RichardIde = "Richard Ide";
-        public const string TimIde = "Tim Ide";
-        public const string GeorgeGray = "George Gray";
 
         // Places
-        public static readonly ISet<string> PlaceTags = new HashSet<string>() { Passumpsic, StJohnsbury };
         public const string Passumpsic = "Passumpsic";
         public const string StJohnsbury = "St. Johnsbury";
-
-        public static string TagIcon(string tagValue)
-        {
-            if (tagValue == TagValues.Featured)
-            {
-                return "tag fas fa-award";
-            }
-            if (TagValues.PersonTags.Contains(tagValue))
-            {
-                return "tag fas fa-user";
-            }
-            if (TagValues.PlaceTags.Contains(tagValue))
-            {
-                return "tag fas fa-map-pin";
-            }
-            return "tag fas fa-tag";
-        }
+        public static readonly ISet<string> PlaceTags = new HashSet<string>() { Passumpsic, StJohnsbury };
     }
 }
