3dc8cfc742fd306981e7fe8b0879755096f8bb7d
Jamie Ide <jide@veic.org>
2019-01-12 12:35:27 -0500

    Added Richard Ide WW2 photos

---
+++ Pages/Images/RichardIdeWorldWar2Photos.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using EtAndHkIde.Infrastructure;
+using System;
+
+namespace EtAndHkIde.Pages.Images
+{
+    public class RichardIdeWorldWar2PhotosModel : SitePageModel
+    {
+        public RichardIdeWorldWar2PhotosModel() : base("Richard Ide World War II Photos")
+        {
+            Description = "A collection of snapshots taken by Richard Ide during World War II service. Most and maybe all these photos were taken in Bremen.";
+            PublishDate = new DateTime(2019, 1, 12);
+            Tags = new[] { TagValues.People.RichardIde };
+        }
+    }
+}
