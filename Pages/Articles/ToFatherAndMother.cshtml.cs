e618c12ae957879a58efcf3b89694a8c082009d4
Jamie Ide <jide@veic.org>
2019-01-11 20:28:37 -0500

    Added to father and mother

---
+++ Pages/Articles/ToFatherAndMother.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using EtAndHkIde.Infrastructure;
+
+namespace EtAndHkIde.Pages.Articles
+{
+    public class ToFatherAndMotherModel : SitePageModel
+    {
+        public ToFatherAndMotherModel() : base("To Father and Mother from Their Girls")
+        {
+            Citation = new Citation(CitationType.Pdf, @"/articles/ToFatherAndMother/To Father And Mother.pdf");
+            Tags = new[] { TagValues.Featured };
+        }
+    }
+}
