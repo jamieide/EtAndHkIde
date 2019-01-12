fd1cbbd3e76e4c24d951f4bc4fc938594563e7bf
Jamie Ide <jide@veic.org>
2019-01-12 10:13:34 -0500

    Added ET family portrait

---
+++ Pages/Images/ElmoreAndCynthiaFamilyPortrait.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EtAndHkIde.Infrastructure;
+
+namespace EtAndHkIde.Pages.Images
+{
+    public class ElmoreAndCynthiaFamilyPortraitModel : SitePageModel
+    {
+        public ElmoreAndCynthiaFamilyPortraitModel() : base("Elmore and Cynthia Ide Family Portrait")
+        {
+            Description = "A family portrait of Elmore and Cynthia Ide circa 1895.";
+            PublishDate = new DateTime(2019, 1, 12);
+            Tags = new[] { TagValues.ElmoreIde, TagValues.CynthiaIde };
+        }
+    }
+}
