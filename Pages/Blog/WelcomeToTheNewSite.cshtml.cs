ccf2dd7c68f9ef959cf57b02ce5d25e78408ab8e
Jamie Ide <jide@veic.org>
2019-01-19 09:32:06 -0500

    Managed to type out two sentences for About page.

---
+++ Pages/Blog/WelcomeToTheNewSite.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EtAndHkIde.Infrastructure;
+
+namespace EtAndHkIde.Pages.Blog
+{
+    public class WelcomeToTheNewSiteModel : SitePageModel
+    {
+        public WelcomeToTheNewSiteModel() : base("Welcome to the new site!")
+        {
+            Description = "Pardon the dust and noise, just because it's open doesn't mean it's finished.";
+            PublishDate = new DateTime(2019, 1, 18);
+        }
+    }
+}
