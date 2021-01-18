3525bcc7f0d3a26976a8b71de1eedf81c1259f5a
Jamie Ide <jamie.ide@gmail.com>
2021-01-18 10:48:29 -0500

    Added VHS photos.

---
+++ Pages/VhsPassumpsicPhotos.cshtml.cs
@@ -0,0 +1,23 @@
+using EtAndHkIde.Infrastructure;
+using System;
+
+namespace EtAndHkIde.Pages
+{
+    public class VhsPassumpsicPhotosModel : SitePageModel
+    {
+        public VhsPassumpsicPhotosModel()
+        {
+            Metadata = new PageMetadata
+            {
+                Title = "Vermont Historical Society Photos",
+                Description = "In 2018, the Vermont Historical Society acquired several cabinet card photographs from a descendant of Francis \"Frank\" Mason.",
+                PublishDate = new DateTime(2021, 1, 18),
+                PreviewImage = "pages/VhsPassumpsicPhotos/vhspassumpsicphotos-preview.jpg"
+            };
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
