ecdfdd8397ea0e9baaa09348f7c7104dd795bd81
Jamie Ide <jamie.ide@gmail.com>
2021-01-24 11:29:53 -0500

    Vermont's oldest family business 1988

---
+++ Pages/VermontsOldestFamilyBusiness1988.cshtml.cs
@@ -0,0 +1,21 @@
+using EtAndHkIde.Infrastructure;
+using System;
+
+namespace EtAndHkIde.Pages
+{
+    public class VermontsOldestFamilyBusiness1988Model : SitePageModel
+    {
+        public VermontsOldestFamilyBusiness1988Model()
+        {
+            Metadata = new PageMetadata
+            {
+                Title = "Vermont's Oldest Family Business: 175th Anniversary",
+                Description = "TODO"
+            };
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
