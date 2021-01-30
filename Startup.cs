0ba5256a0918dacf29526b742b96bee64ff1825c
Jamie Ide <jamie.ide@gmail.com>
2021-01-30 14:32:59 -0500

    Vermont's oldest family business

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,13 @@
 ﻿using EtAndHkIde.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Net;
 
 namespace EtAndHkIde
@@ -13,8 +16,21 @@ namespace EtAndHkIde
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            // TODO response caching
             // https://stackoverflow.com/a/58657997/12752
-            services.AddRazorPages().AddRazorRuntimeCompilation().AddNewtonsoftJson();
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation()
+                //.AddMvcOptions(options =>
+                //{
+                //    var defaultCacheAttribute = new ResponseCacheAttribute
+                //    {
+                //        Duration = 86400, // 1 day
+                //        Location = ResponseCacheLocation.Any
+                //    };
+                //    options.Filters.Add(defaultCacheAttribute);
+                //})
+                .AddNewtonsoftJson();
+            //services.AddResponseCaching();
             services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
             services.AddAntiforgery();
             services.AddSingleton<ISiteRepository, SiteRepository>();
@@ -31,6 +47,17 @@ namespace EtAndHkIde
 
             app.UseStatusCodePagesWithReExecute("/Status{0}");
             app.UseStaticFiles();
+            //app.UseResponseCaching();
+            //app.Use(async (context, next) =>
+            //{
+            //    context.Response.GetTypedHeaders().CacheControl =
+            //    new Microsoft.Net.Http.Headers.CacheControlHeaderValue
+            //    {
+            //        Public = true,
+            //        MaxAge = TimeSpan.FromDays(1),
+            //    };
+            //    await next();
+            //});
 
             // site re-org 12/5/20, everything is just a page now
             // TODO change to permanent when working
